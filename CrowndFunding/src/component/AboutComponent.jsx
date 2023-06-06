af7aa8a0179c29eef91f4a84b1cca69b312a71da
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-06 22:28:29 +0700

    Ch9 add CardComponent & datacard

--- CrowndFunding/src/component/AboutComponent.jsx
+++ CrowndFunding/src/component/AboutComponent.jsx
@@ -1,9 +1,28 @@
-
+import CardComponent from "./CardComponent"
 
 const AboutComponent = () => {
+  const person=[
+    { topicname:"Bamboo Stand", 
+      pledge:"Pledge $25 or more", 
+      paragraph:"You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+      count:101     
+    },
+    {
+      topicname:"Black Edition Stand", 
+      pledge:"Pledge $75 or more", 
+      paragraph:"You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      count:64
+    },
+    {
+      topicname:"Mahogany Special Edition", 
+      pledge:"Pledge $200 or more", 
+      paragraph:"You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      count:0
+    }
+  ]
   return (
-    <section className=" bg-white rounded-lg  px-5 py-8 mt-6
-                        sm:px-10 sm:py-10">
+    <section className=" bg-white rounded-lg  px-5 pt-8 py-4 mt-6
+                        sm:px-10 sm:pt-10 ">
         <h1 className=" text-[1.3rem] font-bold mb-8">About this project</h1>
         <div className="text-[text-[#7a7a7a]]">
             <p className="mb-8 ">
@@ -16,10 +35,13 @@ const AboutComponent = () => {
             to allow notepads, pens, and USB sticks to be stored under the stand.
             </p>
         </div>
-        
+        <CardComponent {...person[0]} key={1} />   
+        <CardComponent {...person[1]} key={2} />  
+        <CardComponent {...person[2]} key={3} /> 
     </section>
 
   )
 }
+
 
 export default AboutComponent
