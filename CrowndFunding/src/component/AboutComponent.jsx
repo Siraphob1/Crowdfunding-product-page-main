68199a8632400cce165e6623c87554a3e6d874f8
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-06 16:07:20 +0700

    Ch9 add AboutComponent

---
+++ CrowndFunding/src/component/AboutComponent.jsx
@@ -0,0 +1,25 @@
+
+
+const AboutComponent = () => {
+  return (
+    <section className=" bg-white rounded-lg  px-5 py-8 mt-6
+                        sm:px-10 sm:py-10">
+        <h1 className=" text-[1.3rem] font-bold mb-8">About this project</h1>
+        <div className="text-[text-[#7a7a7a]]">
+            <p className="mb-8 ">
+            The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen 
+            to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve 
+            your posture and make you more comfortable while at work, helping you stay focused on the task at hand.
+            </p>
+            <p className="mb-8">
+            Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer 
+            to allow notepads, pens, and USB sticks to be stored under the stand.
+            </p>
+        </div>
+        
+    </section>
+
+  )
+}
+
+export default AboutComponent
