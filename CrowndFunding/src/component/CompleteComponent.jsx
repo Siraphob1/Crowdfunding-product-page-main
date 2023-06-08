39b1f7964f94e10f0a6ea8bf490a319342ff824d
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-08 12:49:23 +0700

    Ch9 add id and reactor button tag to a tag

--- CrowndFunding/src/component/CompleteComponent.jsx
+++ CrowndFunding/src/component/CompleteComponent.jsx
@@ -19,7 +19,8 @@ const CompleteComponent = () => {
                                       :"hidden"}>
         <div className="bg-mordal-dark z-50 "></div>
         <div className=" absolute z-[51]  top-[9rem] left-1/2 -translate-x-1/2 w-11/12   bg-white rounded-lg text-center px-4 pt-8 py-12
-                        xl:top-[30rem] xl:w-[37%] xl:px-10 xl:pt-10 xl:pb-12">
+                        xl:top-[30rem] xl:w-[37%] xl:px-10 xl:pt-10 xl:pb-12"
+            id="thankyou">
             <img src={icheck} alt="icheck" className="mx-auto" />
             <h1 className="my-4 text-[1.25rem] font-bold
                             xl:mt-12">Thanks for your support!</h1>
@@ -27,7 +28,7 @@ const CompleteComponent = () => {
                             xl:mt-4"> Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
                 an email once our campaign is completed.
             </p>
-            <button className="btn-click mx-auto" onClick={ShowPagethankyou}>Got it!</button>
+            <a href="#" className="btn-click mx-auto" onClick={ShowPagethankyou}>Got it!</a>
         </div>
     </section>
   )
