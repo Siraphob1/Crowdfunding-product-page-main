da924f778648955db776a3d0b7fcc10c30d0b1a6
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-08 12:48:27 +0700

    Ch9 Fix top responsive

--- CrowndFunding/src/component/MainComponent.jsx
+++ CrowndFunding/src/component/MainComponent.jsx
@@ -6,8 +6,8 @@ import TopicComponent from "./TopicComponent";
 
 const MainComponent=()=>{
     return(
-        <main className=" absolute z-[1] top-60 left-1/2 -translate-x-1/2 w-11/12 py-[5rem]
-                          sm:w-2/4">
+        <main className=" absolute z-[1] top-40 left-1/2 -translate-x-1/2 w-11/12 py-[5rem]
+                          sm:w-2/4 sm:top-20 lg:top-40 xl:top-52">
         <TopicComponent/>   
         <PriceComponent/>   
         <AboutComponent/>   
