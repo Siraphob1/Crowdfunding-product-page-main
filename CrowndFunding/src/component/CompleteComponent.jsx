cbad095539c635117b1b695a4f33e88502504dde
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-08 12:03:11 +0700

    Ch9 use context & Func

--- CrowndFunding/src/component/CompleteComponent.jsx
+++ CrowndFunding/src/component/CompleteComponent.jsx
@@ -1,12 +1,25 @@
+import { useContext } from "react";
+import DataContext from "../Data/DataContext";
 import icheck from "../images/icon-check.svg";
 
 
 const CompleteComponent = () => {
+
+  const [btnBackthisproject,setbtnBackthisproject,person,pagethankyou,setPagethankyou] = useContext(DataContext)
+
+  const ShowPagethankyou=()=>{
+    const currentclick = !pagethankyou
+    setPagethankyou(currentclick)
+  }
+
+  
+
   return (
-    <section>
+    <section className={pagethankyou  ?""
+                                      :"hidden"}>
         <div className="bg-mordal-dark z-50 "></div>
         <div className=" absolute z-[51]  top-[9rem] left-1/2 -translate-x-1/2 w-11/12   bg-white rounded-lg text-center px-4 pt-8 py-12
-                        xl:top-[30rem] xl:w-2/5 xl:px-16 xl:pt-10 xl:pb-16">
+                        xl:top-[30rem] xl:w-[37%] xl:px-10 xl:pt-10 xl:pb-12">
             <img src={icheck} alt="icheck" className="mx-auto" />
             <h1 className="my-4 text-[1.25rem] font-bold
                             xl:mt-12">Thanks for your support!</h1>
@@ -14,7 +27,7 @@ const CompleteComponent = () => {
                             xl:mt-4"> Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
                 an email once our campaign is completed.
             </p>
-            <button className="btn-click mx-auto">Got it!</button>
+            <button className="btn-click mx-auto" onClick={ShowPagethankyou}>Got it!</button>
         </div>
     </section>
   )
