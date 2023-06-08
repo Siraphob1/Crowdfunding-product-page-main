569064cb4740d3e112d8842fde8a2e1c0dcea700
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-08 12:01:18 +0700

    Ch9 add id about&getstart

--- CrowndFunding/src/component/AboutComponent.jsx
+++ CrowndFunding/src/component/AboutComponent.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import CardComponent from "./CardComponent"
 import DataContext from "../Data/DataContext"
 
@@ -7,8 +7,9 @@ const AboutComponent = () => {
   const [btnBackthisproject,setbtnBackthisproject,person] = useContext(DataContext);
   
   return (
-    <section className=" bg-white rounded-lg  px-5 pt-8 py-4 mt-6
-                        sm:px-10 sm:pt-10 ">
+    <section className=" bg-white rounded-lg  px-5 pt-8 py-4 mt-6 
+                        sm:px-10 sm:pt-10 "
+              id="about">
         <h1 className=" text-[1.3rem] font-bold mb-8">About this project</h1>
         <div className="text-[text-[#7a7a7a]]">
             <p className="mb-8 ">
@@ -16,7 +17,7 @@ const AboutComponent = () => {
             to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve 
             your posture and make you more comfortable while at work, helping you stay focused on the task at hand.
             </p>
-            <p className="mb-8">
+            <p className="mb-8" id="getstart">
             Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer 
             to allow notepads, pens, and USB sticks to be stored under the stand.
             </p>
